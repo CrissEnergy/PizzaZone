8cef7e16bc75d1bd077aa024bbcad43d2dab4493
Criss Energy <crissenergy1@gmail.com>
2025-11-17 00:21:48 +0000

    and add zoom out animation too

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -96,18 +96,21 @@ export default {
           },
         },
         'zoom-in': {
-          from: {
+          '0%': {
             transform: 'scale(1)',
           },
-          to: {
+          '50%': {
             transform: 'scale(1.1)',
+          },
+          '100%': {
+            transform: 'scale(1)',
           },
         },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'zoom-in': 'zoom-in 6s ease-in-out',
+        'zoom-in': 'zoom-in 8s ease-in-out',
       },
     },
   },
