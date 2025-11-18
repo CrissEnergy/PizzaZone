30710f38d3c991f3780dd776bb1bc4d1d512b7d1
Criss Energy <crissenergy1@gmail.com>
2025-11-18 07:40:06 +0000

    Add amazing( extradionaty) animation on the image ontop the  Our Feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -118,7 +118,7 @@ export default function Home() {
       {/* Featured Items Section */}
       <section className="w-full">
         <div className="container px-4 sm:px-6">
-          <div className="mb-4 flex justify-center">
+          <div className="mb-4 flex justify-center holographic-image">
             <Image
               src="https://lh3.googleusercontent.com/geougc-cs/AMBA38uOmXRmeig7oxlUmMWZfyqAJNn_3GCEC6wixEM1aoyp9cId70HxwbpzKVpa8S_jKfEmp1duI2idx2xFsO3mvzRzZazS3oPVw4UC76qulOe6rAJQC8vZN4_7bXOCDLuq1PQdvK9PjsOYgYNO=w478-h269-p"
               alt="Featured Items separator"
